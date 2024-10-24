3a82c4bdb34e039c8db3aaca6def3e7adb7c091a
Francis Roberts <francisroberts@live.com>
2024-10-24 17:45:42 +0100

    fix build

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import TestimonialsAvatars from "./TestimonialsAvatars";
-import config from "@/config";
 
 const Hero = () => {
+  const someVariable = "This is an example of using &apos;single quotes&apos; in JSX.";
+
   return (
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
@@ -17,10 +18,10 @@ const Hero = () => {
         </a>
 
         <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
-          Education & training providers
+          Education &amp; training providers
         </h1>
         <p className="text-lg opacity-80 leading-relaxed">
-        APST's offer Uniformed Protective Services qualifications & Duke of Edinburgh (DofE) awards.
+          APST&apos;s offer Uniformed Protective Services qualifications &amp; Duke of Edinburgh (DofE) awards.
         </p>
 
         <TestimonialsAvatars priority={true} />
